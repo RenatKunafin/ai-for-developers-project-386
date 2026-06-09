2f390f867617aaf87a63b2ae9fcbd1d186389132
Renat Kunafin <kunafin.renat@yandex.ru>
2026-06-09 16:12:20 +0300

    chore: add TanStack Query DevTools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { theme } from './theme';
 import { Layout } from './components/Layout';
 import { GuestPage } from './pages/GuestPage';
@@ -24,6 +25,7 @@ function App() {
           </Routes>
         </BrowserRouter>
       </MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
